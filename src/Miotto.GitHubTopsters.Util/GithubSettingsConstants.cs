49cba67288099d89ac344b2cb6f511e4a503e0b8
Cintya Miotto <cintyadallacosta@gmail.com>
2023-03-04 15:09:10 -0300

    changes

---
+++ src/Miotto.GitHubTopsters.Util/GithubSettingsConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace Miotto.GitHubTopsters.Util
+{
+    public static class GithubSettingsConstants
+    {
+        public const string BaseUrl = "https://api.github.com";
+        public const string SearchQuery = "search/repositories?type=Repositories&per_page={0}&sort=stars&order=desc&q=+language:{1}";
+        public const int QuantityDefault = 3;
+        public static readonly List<string> LanguagesDefault = new List<string>()
+        {
+            "C#",
+            "Vue",
+            "TypeScript",
+            "JavaScript",
+            "Python"
+        };
+    }
+}
